29b7325638cc9ba373441bce3c7571672348d043
Amenybn <amanibennaceur123@gmail.com>
2026-02-02 11:13:23 +0100

    Initial commit

---
+++ src/main/java/com/backend/devsecopsplatform_backend/DevSecOpsPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.devsecopsplatform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSecOpsPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevSecOpsPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/devsecopsplatform_backend/DevSecOpsPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.devsecopsplatform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSecOpsPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
